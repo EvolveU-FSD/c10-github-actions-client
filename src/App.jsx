2b444465437cacba41e382cd40e86a9ec02f8025
Danielle Barker - Mac <caseybarker86@gmail.com>
2023-05-31 11:55:12 -0600

    change text in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          The count from the server is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
